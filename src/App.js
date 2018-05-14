431499aee4d3edecc36899f0759b66f071014127
davjdo <davido_204@hotmail.com>
2018-05-14 19:34:59 +0200

    adding css modules configuration

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import styles from './App.css';
 
 const App = props => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h1 className="App-title">Welcome to React</h1>
+  <div className={styles.App}>
+    <header className={styles.header}>
+      <img src={logo} className={styles.logo} alt="logo" />
+      <h1 className={styles.title}>Welcome to React</h1>
     </header>
-    <p className="App-intro">
+    <p className={styles.intro}>
       To get started, edit <code>src/App.js</code> and save to reload.
     </p>
   </div>
