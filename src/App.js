c56319c0e7a06a1bfbf233837daf301c7f2b4fc9
davjdo <davido_204@hotmail.com>
2018-05-21 19:33:08 +0200

    adding react-router proptypes axios dependencies and fixing structure react app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
-import Layout from './components/Layout/Layout';
+import Layout from './hoc/Layout/Layout';
 
 const App = props => (
   <div>
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Aux from '../../hoc/Aux';
+import Aux from '../Aux/Aux';
 
 const Layout = props => (
   <Aux>
