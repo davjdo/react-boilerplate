95d565232ed9202e1cc132191fc5a5920d4291c5
davjdo <davido_204@hotmail.com>
2018-05-22 13:03:53 +0200

    fixing linting and scss test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
 import Layout from './hoc/Layout/Layout';
+import styles from './App.scss';
 
 const App = props => (
-  <div>
+  <div className={styles.name}>
     <Layout>
       <p>Test</p>
     </Layout>
